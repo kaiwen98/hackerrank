3b5d0e1aa5d89dcc66b9ffd5d6b7f31337672315
Looi Kai Wen <e0406836@u.nus.edu>
2021-05-30 01:54:25 +0800

    Neat up code

--- attributeparser/main.cpp
+++ attributeparser/main.cpp
@@ -15,25 +15,37 @@ typedef struct {
 } Tag;
 
 void populate_tag(Tag* tag, string input) {
+    // Parses an attribute string: "att = value"
     regex attr_regex("\\S+\\s*=\\s*\"\\S+\"");
+    // Parse different if no attributes exist
     if (!regex_search(input, attr_regex)) {
-        string name = (input.substr(1, input.find(">") - 1));
+        string name = (input.substr(
+            input.find("<") + 1, 
+            input.find(">") - 1));
+
         tag->tag_name = name;
         return;
     }
-    tag->tag_name = input.substr(1, input.find(' ') - 1);
+
+    tag->tag_name = input.substr(
+        input.find("<") + 1, 
+        input.find(' ') - 1);
     //cout << tag->tag_name << endl;
     auto words_begin = sregex_iterator(input.begin(), input.end(), attr_regex);
     auto words_end = sregex_iterator();
     string attribute;
     string value;
+    string substring;
     for (auto i = words_begin; i != words_end; ++i) {
         attribute = i->str().substr(0, i->str().find("="));
-        value = i->str().substr(i->str().find('"') + 1, i->str().find('"', i->str().find('"') + 1) 
-                        - 1 - i->str().find('"'));
         attribute.erase(attribute.find_last_not_of(" \n\r\t")+1);
+
+        substring = i->str().substr(i->str().find("= ") + 2);
+        substring = substring.erase(substring.find_last_not_of(" \n\r\t")+1);
+
+        value = substring.substr(substring.find('"') + 1, substring.find('"', 1) - 1);
         value.erase(value.find_last_not_of(" \n\r\t")+1);
-        //cout << "attr: " << attribute << "value: " << value << endl; 
+
         tag->attributes->insert(pair<string, string>(attribute, value));
     }
 }
@@ -48,13 +60,11 @@ bool handle_input(string input, stack<Tag*> *tag_recursive) {
     if (input[0] ==  '<') {
         if (check_is_close_tag(input)) {
             tag_recursive->pop();
-            //cout << "poped: " << tag_recursive->top()->tag_name << endl;
             return true;
         }
         populate_tag(new_tag, input);
         tag_recursive->top()->children->insert(pair<string, void*>(new_tag->tag_name, (void*)new_tag));
         tag_recursive->push(new_tag);
-        //cout << "pushed: " << tag_recursive->top()->tag_name << endl;
 
         return true;
     }
@@ -62,7 +72,6 @@ bool handle_input(string input, stack<Tag*> *tag_recursive) {
 }
 
 bool handle_query(Tag * root, string input, string &result) {
-    //cout << "start!" << endl;
     int initial_pos = 0;
     int end_pos = 0;
     int length = 0;
@@ -71,13 +80,15 @@ bool handle_query(Tag * root, string input, string &result) {
     Tag *curr_tag = root;
     string curr_attribute;
     int count = 0;
+
     while (1) {
         initial_pos = 0;
         end_pos = temp.find(".");
-        //cout << "temp: " << temp << endl;
+
+        // Proceed to access attribute if valid
         if (end_pos == string::npos) {
-            //cout << "found"<<endl;
             end_pos = temp.find("~");
+
             tag = temp.substr(initial_pos, end_pos);
             try {
                 curr_tag = (Tag*) curr_tag->children->at(tag);
@@ -85,10 +96,9 @@ bool handle_query(Tag * root, string input, string &result) {
                 cout << "Not Found!" << endl;
                 return false;
             }
+
             curr_attribute = temp.substr(end_pos + 1);
-            //cout << "tag: " << tag << endl;
-            
-            //cout << "tag1: " << curr_tag->tag_name << endl;
+
             if (!tag.compare(curr_tag->tag_name)) {
                 try {
                     curr_tag->attributes->at(curr_attribute);
@@ -96,16 +106,14 @@ bool handle_query(Tag * root, string input, string &result) {
                     cout << "Not Found!" << endl;
                     return false;
                 }
-                //cout << tag_vect->at(count)->attributes->at(temp.substr(end_pos + 1)) << " here" << endl;
                 result = curr_tag->attributes->at(temp.substr(end_pos + 1));
                 cout << result << endl;
             }
             return true;
         } else {
-            length = end_pos - initial_pos;
-            //cout << length << "  ";
-            tag = temp.substr(initial_pos, length);
-            //cout << "TAG: " << tag << endl;
+            // Proceed to child tag if valid
+            
+            tag = temp.substr(initial_pos, end_pos);
             temp = temp.substr(end_pos + 1);
             try {
                 curr_tag = (Tag*) curr_tag->children->at(tag);            
@@ -113,8 +121,6 @@ bool handle_query(Tag * root, string input, string &result) {
                 cout << "Not Found!" << endl;
                 return false;
             }
-            
-            //cout << curr_tag->tag_name << endl;
             count++;
         }
     }
@@ -130,6 +136,7 @@ int main() {
     int num_queries;
     string input;
     string result;
+    // Need to keep record of stack frames due to recursive call of a tag from a parent tag.
     stack<Tag*> *tag_recursive = new stack<Tag*>();
     tag_recursive->push(root);
     cin >> num_lines >> num_queries;
@@ -137,19 +144,12 @@ int main() {
     while (num_lines) {
         getline(cin, input);
         if(handle_input(input, tag_recursive)) {
-            //cout << num_lines << input << endl;
             num_lines--;
         }
     }
 
     vector<string> key;
 
-    // for(auto i : *(tag_vect->at(0)->attributes)) {
-    //     key.push_back(i.first);
-    //     //cout << "Key: " << i.first << endl;
-    // }
-    //cout << "so far" << endl;
-    //cout << "size: " << ((Tag*)((Tag*)root->children->at("a"))->children->at("b"))->tag_name << endl;
     while (num_queries) {
         getline(cin, input);
         handle_query(root, input, result);
